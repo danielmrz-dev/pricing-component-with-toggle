4010eb42646bcd8b2b373ce2f494fb27c0bef1e2
Daniel Moraes Mariz <137360936+danielmrz-dev@users.noreply.github.com>
2024-04-16 18:10:29 -0300

    javascript acessibility

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,24 @@
+const checkboxLabel = document.querySelector(".header__switch");
 const togglePriceBtn = document.querySelector("[data-toggle-btn]");
 const prices = document.querySelectorAll("[data-price]");
 
 function convertedPrice(priceElement, conversionFactor) {
     const priceNumber = Number(priceElement.innerText);
-    const convertedPrice = (priceNumber * conversionFactor).toFixed(2)
-    priceElement.innerText = convertedPrice
+    const convertedPrice = (priceNumber * conversionFactor).toFixed(2);
+    priceElement.innerText = convertedPrice;
 }
 
 togglePriceBtn.addEventListener("change", () => {
     const conversionFactor = togglePriceBtn.checked ? 0.1 : 10;
-    prices.forEach(price => convertedPrice(price, conversionFactor))
-})
+    prices.forEach((price) => convertedPrice(price, conversionFactor));
+});
+
+checkboxLabel.addEventListener("keydown", (event) => {
+    if (event.key === "Enter" || event.key === " ") {
+        event.preventDefault();
+        togglePriceBtn.checked = !togglePriceBtn.checked;
+        const conversionFactor = togglePriceBtn.checked ? 0.1 : 10;
+        prices.forEach((price) => convertedPrice(price, conversionFactor));
+    }
+});
+
